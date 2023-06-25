e5ac58a17a10dabf2800c8f07e2c05b862b67724
George Cosmin Taschina <georgecosmintaschina1631@gmail.com>
2023-06-26 01:28:01 +0200

    rivelo page

--- src/app/Projects.tsx
+++ src/app/Projects.tsx
@@ -14,10 +14,10 @@ type ProjectItem = {
 const Projects = () => {
     
     const projects: ProjectItem[] = [
-        { src: '/rivelo-project.jpeg', alt: 'Rivelo Platform', title: 'Rivelo Platform', description: 'Rivelo is a platform that allows you to create your own online store in a few minutes and start selling your products online.', link: '/rivelo', imageMerit: 'Photo by Mihai Vlasceanu from Pexels' },
-        { src: '/wifi-project.jpeg', alt: 'Wifi Italia', title: 'Wifi Italia', description: 'B4web is a platform that allows you to create your own online store in a few minutes and start selling your products online.', link: '/wifi', imageMerit: 'Photo by CoWomen from Pexels' },
-        { src: '/redbissi-project.jpeg', alt: 'RedAbissi Platform', title: 'RedAbissi Platform', description: 'IdeaEncode is a platform that allows you to create your own online store in a few minutes and start selling your products online.', link: '/redabissi', imageMerit: 'Photo by Amina Filkins from Pexels' },
-        { src: '/bikerface-project.jpeg', alt: 'Bikerface', title: 'Bikerface', description: 'RedAbissi is a platform that allows you to create your own online store in a few minutes and start selling your products online.', link: '/bikerface', imageMerit: 'Photo by Nathan Salt from Pexels' },
+        { src: '/rivelo-project.jpeg', alt: 'Rivelo Platform', title: 'Rivelo Platform', description: 'Rivelo is a cutting-edge SaaS Planning and Control (FP&A) platform that enhances economic and financial performances, revealing new business opportunities. Serving numerous companies across various sectors', link: '/rivelo', imageMerit: 'Photo by Mihai Vlasceanu from Pexels' },
+        { src: '/wifi-project.jpeg', alt: 'Wifi Italia', title: 'Wifi Italia', description: 'WiFi Italia is the project of the Ministry of Enterprises and Made in Italy that allows citizens to connect easily and for free with an APP to the free and widespread WiFi network in Italian squares, in small villages, and in the city.', link: '/wifi', imageMerit: 'Photo by CoWomen from Pexels' },
+        { src: '/redbissi-project.jpeg', alt: 'RedAbissi Platform', title: 'RedAbissi Platform', description: 'RedAbissi is a Social media management platform, developed for brands and companies selling through single-brand & multi-brand stores, franchises, networks, agencies, partners and retailers.', link: '/redabissi', imageMerit: 'Photo by Amina Filkins from Pexels' },
+        { src: '/bikerface-project.jpeg', alt: 'Bikerface', title: 'Bikerface', description: 'Bikerface Transport is a premier motorcycle transport company dedicated to providing safe, reliable, and timely motorcycle transportation services globally.', link: '/bikerface', imageMerit: 'Photo by Nathan Salt from Pexels' },
     ];
 
     return (
--- src/app/rivelo/page.tsx
+++ src/app/rivelo/page.tsx
@@ -1,14 +1,38 @@
+import BrandCarousel from "@/components/BrandCarousel";
 
+const Home = () => {
+    type CarouselItem = {
+        src: string;
+        alt: string;
+    };
 
-const Home =() => {
+    const brands: CarouselItem[] = [
+        { src: '/c-sharp.png', alt: 'C-sharp' },
+        { src: '/gcp.png', alt: 'GCP' },
+        { src: '/k8s.png', alt: 'K8s' },
+        { src: '/react.png', alt: 'React' },
+        { src: '/kafka.webp', alt: 'Kafka' },
+        { src: '/laravel.png', alt: 'Laravel' },
+        { src: '/mysql.png', alt: 'MySQL' },
+      ];
+
     return (
-      <main>
-          <div className="h-screen flex justify-center items-center">
-              Hey
-          </div>
-      </main>
+        <main>
+            <div className="h-screen">
+                <div className="parallax"></div>
+                <div className="grid grid-cols-2 gap-4 h-1/2">
+                    <div className="flex justify-center items-center">                
+                        <h1 className="text-2xl md:text-5xl font-bold text-special">Rivelo Platform</h1>
+                    </div>
+                    <div className="flex justify-center items-center flex-col container">
+                        <p className="font-bold text-xl md:text-2xl">Revolutionizing the Digital Landscape: My Journey with Rivelo Platform</p>
+                        <p className="text-md md:text-xl text-special mt-3">Throughout my time at Rivelo, I've undertaken several vital projects that have significantly contributed to the overall performance, security, and efficiency of our digital environment. Let's delve deeper into the specifics of my role and the impact of my contributions.</p>
+                    </div>
+                </div>
+            </div>
+            <BrandCarousel items={brands} className={"my-7"}/>
+        </main>
     )
-  }
-  
-  export default Home
-  
+}
+
+export default Home
